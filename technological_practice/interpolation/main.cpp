a5754eb1c94cb28f926a0fdc84cf838a8133c80d
tulen-pod-soysom <al.kovschickov@gmail.com>
2024-11-01 20:12:44 +0300

    technological_practice

--- technological_practice/interpolation/main.cpp
+++ technological_practice/interpolation/main.cpp
@@ -1,38 +1,81 @@
 #include <armadillo>
+#include <boost/gil/image_view.hpp>
+#include <boost/gil/typedefs.hpp>
+#include <boost/gil/extension/io/bmp.hpp>
+#include <boost/gil.hpp>
+
 #include <cstdlib>
 #include <fstream>
 
 #include "bilinear_interpolation.hpp"
 #include "biquadratic_interpolation.hpp"
 
-int main()
-{
-    arma::uchar_mat im(10,10);
+#include <random>
 
-    for (auto i =0; i < im.n_rows; ++i) {
-        for (auto j =0 ; j < im.n_cols; ++j)
-        {
-            // im(i,j) = double(rand() )* 255/ RAND_MAX;
-            im(i,j) = 255.0/2.0*(sin(2*3.1415926535*(i+j)/10.0)+1);
-        }
+template <typename T>
+auto sqr(T a)
+{
+    return a * a;
+}
+
+template <typename T>
+T sqr(std::complex<T> a)
+{
+    return a.imag() * a.imag() + a.real() * a.real();
+}
+
+template <typename Matrix1, typename Matrix2>
+auto MSE(Matrix1& m1, Matrix2& m2, unsigned rows, unsigned cols)
+{
+    double error = 0.0;
+    for (auto i = 0; i < rows; ++i)
+        for (auto j = 0; j < cols; ++j)
+    {
+        error += sqr(m1(i,j) - m2(i,j));
     }
 
-    unsigned width, height;
-    std::cin >> width >> height;
+    return error /(double)rows /(double)cols;
+}
 
-#ifdef BILINEAR_INTERPOLATION
-    arma::uchar_mat res = bilinear_interpolation<arma::uchar_mat>(im, im.n_cols, im.n_rows, width,height);
-    std::ofstream f2("res_bilinear.txt");
-#endif
+template <typename T1, typename T2>
+auto MSE(arma::Mat<T1>& m1, arma::Mat<T2>& m2)
+{
+    assert(m1.n_cols == m2.n_cols);
+    assert(m1.n_rows == m2.n_rows);
+    return MSE(m1,m2,m1.n_rows,m1.n_cols);
+}
 
-// #ifdef BIQUADRATIC_INTERPOLATION
-    arma::uchar_mat res = biquadratic_interpolation<arma::uchar_mat>(im, im.n_cols, im.n_rows, width,height);
-    res = biquadratic_interpolation<arma::uchar_mat>(res, im.n_cols, im.n_rows, 10,10);
-    std::ofstream f2("res_biquadratic.txt");
-// #endif
+int main()
+{
+    double k_max = 4;
 
-    std::ofstream f1("im.txt");
+    std::string filename; std::cin >> filename;
 
-    im.print(f1);
-    res.print(f2);
+    arma::uchar_mat im = gray_image_from_file(filename);
+
+    auto w = im.n_rows;
+    auto h = im.n_cols;
+
+    auto bilinear = im;
+    auto biquadratic = im;
+
+    unsigned n_experiments = 7;
+
+    std::random_device rd;
+    std::uniform_real_distribution<double> dist(0,k_max);
+
+    for (auto i =0; i < n_experiments; ++i)
+    {
+        unsigned new_w = w * dist(rd);
+        unsigned new_h = h * dist(rd);
+
+        bilinear = bilinear_interpolation<arma::uchar_mat>(bilinear, bilinear.n_rows, bilinear.n_cols , new_w, new_h);
+        biquadratic = biquadratic_interpolation<arma::uchar_mat>(biquadratic, biquadratic.n_rows, biquadratic.n_cols , new_w, new_h);
+    }
+
+    bilinear = bilinear_interpolation<arma::uchar_mat>(bilinear, bilinear.n_rows, bilinear.n_cols , w, h);
+    biquadratic = biquadratic_interpolation<arma::uchar_mat>(biquadratic, biquadratic.n_rows, biquadratic.n_cols ,w,h);
+
+    std::cout << "bilinear MSE: " << MSE(im,bilinear) << std::endl;
+    std::cout << "biquadratic MSE: " << MSE(im,biquadratic) << std::endl;
 }
--- technological_practice/interpolation/visualization/main.cpp
+++ technological_practice/interpolation/visualization/main.cpp
@@ -7,18 +7,22 @@
 
 int main()
 {
-    arma::uchar_mat im(10,10);
+
+    unsigned init_width, init_height;
+    unsigned width, height;
+
+    std::cin >> init_width >> init_height;
+    std::cin >> width >> height;
+
+    arma::uchar_mat im(init_width,init_height);
 
     for (auto i =0; i < im.n_rows; ++i) {
         for (auto j =0 ; j < im.n_cols; ++j)
         {
             // im(i,j) = double(rand() )* 255/ RAND_MAX;
-            im(i,j) = 255.0/2.0*(sin(2*3.1415926535*(i+j)/10.0)+1);
+            im(i,j) = 255.0/2.0*(sin(2*3.1415926535*(i+j)/double(init_width))+1);
         }
     }
-
-    unsigned width, height;
-    std::cin >> width >> height;
 
 #ifdef BILINEAR_INTERPOLATION
     arma::uchar_mat res = bilinear_interpolation<arma::uchar_mat>(im, im.n_cols, im.n_rows, width,height);
